071edbe1835d91455e84bb3f534bb6df9461a480
Adam <adam.nemmish213@gmail.com>
2024-08-03 17:10:21 +0600

    Выполнил 4 этап

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accounts.views import RegistrationView
+from accounts.views import RegistrationView, UserDetailView
 
 app_name = 'accounts'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegistrationView.as_view(), name='register'),
+    path('profile/<int:pk>/', UserDetailView.as_view(), name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login, get_user_model
 from django.shortcuts import redirect, reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from accounts.forms import MyUserCreationForm
 
 
@@ -21,3 +21,15 @@ class RegistrationView(CreateView):
         if not next_page:
             next_page = reverse('webapp:home')
         return next_page
+
+
+class UserDetailView(DetailView):
+    template_name = 'user_detail.html'
+    model = get_user_model()
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context['answer_count'] = user.author_answers.count()
+        return context
